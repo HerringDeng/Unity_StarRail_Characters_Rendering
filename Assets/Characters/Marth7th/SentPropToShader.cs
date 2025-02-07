d737eb15f7b103179b5ec997e582908dc2781140
HerringDeng <1640218559@qq.com>
2025-02-08 00:29:19 +0800

    添加了天空盒子，修改了脸部sdf的uv变换方式

---
+++ Assets/Characters/Marth7th/SentPropToShader.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SentPropToShader : MonoBehaviour
+{
+    public Transform headCenter;
+    public Transform headForward;
+    public Transform headRight;
+    private Renderer[] allRenderers;
+    private int headForwardID = Shader.PropertyToID("_HeadForwardVector");
+    private int headRightID = Shader.PropertyToID("_HeadRightVector");
+        private int headUpID = Shader.PropertyToID("_HeadUpVector");
+
+// #if UNITY_EDITOR
+//     void OnValidate()
+//     {
+//         LateUpdate();
+//     }
+// #endif
+
+    void LateUpdate()
+    {
+        if(allRenderers == null)
+        {
+            allRenderers = GetComponentsInChildren<Renderer>(true);
+        }
+        for(int i = 0; i < allRenderers.Length; i++)
+        {
+            Renderer r = allRenderers[i];
+            foreach(Material mat in r.sharedMaterials)
+            {
+                if(mat.shader)
+                {
+                    if(mat.shader.name == "Unlit/SR_Character_Shader")
+                    {
+                        Vector3 currHeadForwardVector = headForward.position-headCenter.position;
+                        Vector3 currHeadRightVector = headRight.position-headCenter.position;
+
+                        mat.SetVector(headForwardID, currHeadForwardVector.normalized);
+                        mat.SetVector(headRightID, currHeadRightVector.normalized);
+                        mat.SetVector(headUpID, Vector3.Cross(currHeadForwardVector, currHeadRightVector).normalized);
+                    }
+                }
+            }
+        }
+    }
+}
