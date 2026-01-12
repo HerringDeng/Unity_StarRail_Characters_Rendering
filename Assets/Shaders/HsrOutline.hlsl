9527070d743ec8e926d872e0bba76b6fee736834
HerringDeng <1640218559@qq.com>
2026-01-12 22:02:50 +0800

    为实现头发半透明眼透效果，将原本的部位域控制的单shader拆分为不同部位多个shader

--- Assets/Shaders/HsrOutline.hlsl
+++ Assets/Shaders/HsrOutline.hlsl
@@ -3,13 +3,25 @@
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
 #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"
 
+#define _OUTLINE_WIDTH_SCALE 0.0009
+
+struct OutlineData
+{
+    float outlineWidth;
+    float outlineZBias;
+    // dynamic outline only below
+    float outlineStandardCameraDistance;
+    float outlineWidthRangeOffset;
+};
+
 struct OutlineAttributes
 {
     float3 positionOS : POSITION;
     float3 normalOS : NORMAL;
     float4 tangentOS : TANGENT;
     float2 uv : TEXCOORD0;
-    float3 outlineNormal : TEXCOORD1;
+    float2 uv1 : TEXCOORD1;
+    float4 color : COLOR;
 };
 
 struct OutlineVaryings
@@ -18,7 +30,7 @@ struct OutlineVaryings
     float4 outlinePostionHCS : SV_POSITION;
 };
 
-float3 UnitOctahedronUVtoNormal(float2 oct, bool negative)
+float3 TransformOctahedronUVtoNormalTS(float2 oct, bool negative)
 {
     if(!negative)
     {
@@ -27,70 +39,99 @@ float3 UnitOctahedronUVtoNormal(float2 oct, bool negative)
     float3 n = float3(oct.xy, 1.0 - abs(oct.x) - abs(oct.y));
     if (n.z < 0.0)
     {
-        // 处理折叠区域
         float2 t = float2(1.0 - abs(n.y), 1.0 - abs(n.x));
         n.xy = (oct.xy >= 0.0) ? t : -t;
     }
     return normalize(n);
 }
 
-OutlineVaryings Vert(OutlineAttributes input)
+float3 TransfromOctahedronUVtoNormalVS(float2 oct, float3x3 tnb, bool negative)
 {
-    OutlineVaryings output;
-    VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS);
-    VertexNormalInputs vertexNormalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
-    float3 outlinePostionWS = vertexInput.positionWS;
-    float3 cameraPostionWS = GetCameraPositionWS();
-    float3 biasDir = normalize(outlinePostionWS - cameraPostionWS);
-    float biasScale = 1000;
-    float3x3 tnb = float3x3(vertexNormalInput.tangentWS, vertexNormalInput.bitangentWS, vertexNormalInput.normalWS);
-    //相机射线方向偏移
-    outlinePostionWS += biasDir / biasScale * _OutlineZBias;
-    //法线
-    float3 normalTS = input.outlineNormal.xyz;
-    normalTS = normalize(UnitOctahedronUVtoNormal(normalTS.xy, false));
-    float3 normalWS = mul(normalTS, tnb);
-    float3 positionVS = TransformWorldToView(outlinePostionWS);
-    float3 normalVS = TransformWorldToViewDir(normalWS);
-    #if _OUTLINE_ON
-        //固定粗细描边模式
-        #if _OUTLINETYPE_FIXED_WIDTH
-            positionVS += normalize(normalVS) * _OutlineWidth * _OutlineWidthScale;
-            output.outlinePostionHCS = TransformWViewToHClip(positionVS);
-        //固定像素描边模式
-        #elif _OUTLINETYPE_FIXED_PIXEL
-            //float3 normalHCS = mul((float3x3)UNITY_MATRIX_VP, normalWS);
-            float3 normalHCS = TransformWorldToHClipDir(normalWS);
-            output.outlinePostionHCS = TransformWorldToHClip(outlinePostionWS);
-            float2 outlineOffset = (_OutlineWidth * output.outlinePostionHCS.w) / (_ScreenParams.xy / 2.0);
-            output.outlinePostionHCS.xy += normalize(normalHCS.xy) * outlineOffset;
-        //随相机距离自动调整的动态宽度描边模式
-        #elif _OUTLINETYPE_DYNAMIC_WIDTH
-            float outlineWidth = _OutlineWidth*_OutlineWidthScale;
-            float cameraDistance = length(outlinePostionWS - cameraPostionWS);
-            outlineWidth = clamp(outlineWidth*cameraDistance, _OutlineMinWidth*_OutlineWidthScale, _OutlineMaxWidth*_OutlineWidthScale);
-            positionVS += normalize(normalVS) * outlineWidth;
-            output.outlinePostionHCS = TransformWViewToHClip(positionVS);
-        #endif
-    #else
-        output.outlinePostionHCS = TransformWorldToHClip(outlinePostionWS);
-    #endif
-    output.uv = input.uv;
-    return output;
+    float3 n = TransformOctahedronUVtoNormalTS(oct, negative);
+    n = mul(n, tnb);
+    n = TransformWorldToViewDir(n);
+    return n;
 }
 
-half4 Frag(OutlineVaryings input) : SV_Target
+float4 CalculateFixedWidthOutlinePostionHCS(OutlineAttributes input, OutlineData data)
 {
-    #if _OUTLINE_OFF
-        clip(-1);
-        return 0;
-    #else
-        #if _AREA_FACE
-            half4 lightMap = SAMPLE_TEXTURE2D(_SdfLightMap, sampler_SdfLightMap, input.uv);
-            half outlineArea = lightMap.r;
-            clip(lerp(1, -1, outlineArea));
-        #endif
-        return _OutlineColor;
-    #endif
+    VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS);
+    VertexNormalInputs vertexNormalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+    float3x3 tnb = float3x3(vertexNormalInput.tangentWS, vertexNormalInput.bitangentWS, vertexNormalInput.normalWS);
+    float3 normalVS = TransfromOctahedronUVtoNormalVS(input.uv1, tnb, false);
+    normalVS.z = -0.01;
+    // calculate camera bias
+    float3 cameraPostionWS = GetCameraPositionWS();
+    float3 biasDirWS = normalize(vertexInput.positionWS - cameraPostionWS);
+    float3 biasWS = biasDirWS * data.outlineZBias/1000;
+    float3 biasPostionWS = vertexInput.positionWS + biasWS;
+    float3 biasPositionVS = TransformWorldToView(biasPostionWS);
+    // calculate outline position
+    float3 outlinePostionVS = biasPositionVS + normalize(normalVS) * data.outlineWidth * _OUTLINE_WIDTH_SCALE * input.color.a;
+    return TransformWViewToHClip(outlinePostionVS);
 }
+
+float4 CalculateFixedPixelOutlinePostionHCS(OutlineAttributes input, OutlineData data)
+{
+    VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS);
+    VertexNormalInputs vertexNormalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+    float3x3 tnb = float3x3(vertexNormalInput.tangentWS, vertexNormalInput.bitangentWS, vertexNormalInput.normalWS);
+    float3 normalVS = TransfromOctahedronUVtoNormalVS(input.uv1, tnb, false);
+    normalVS.z = -0.01;
+    // calculate camera bias
+    float3 cameraPostionWS = GetCameraPositionWS();
+    float3 biasDirWS = normalize(vertexInput.positionWS - cameraPostionWS);
+    float3 biasWS = biasDirWS * data.outlineZBias/1000;
+    float3 biasPostionWS = vertexInput.positionWS + biasWS;
+    float4 biasPostionHCS = TransformWorldToHClip(biasPostionWS);
+    // calculate outline position
+    float3 normalWS = TransformViewToWorldDir(normalVS);
+    float3 normalHCS = TransformWorldToHClipDir(normalWS);
+    float2 outlineOffset = (data.outlineWidth * biasPostionHCS.w * input.color.a) / (_ScreenParams.xy / 2.0);
+    float4 outlinePostionHCS = biasPostionHCS;
+    outlinePostionHCS.xy += normalize(normalHCS.xy) * outlineOffset;
+    return outlinePostionHCS;
+}
+
+float4 CalculateDynamicWidthOutlinePostionHCS(OutlineAttributes input, OutlineData data)
+{
+    VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS);
+    VertexNormalInputs vertexNormalInput = GetVertexNormalInputs(input.normalOS, input.tangentOS);
+    float3x3 tnb = float3x3(vertexNormalInput.tangentWS, vertexNormalInput.bitangentWS, vertexNormalInput.normalWS);
+    float3 normalVS = TransfromOctahedronUVtoNormalVS(input.uv1, tnb, false);
+    normalVS.z = -0.01;
+    // calculate camera bias
+    float3 cameraPostionWS = GetCameraPositionWS();
+    float cameraDistance = length(vertexInput.positionWS - cameraPostionWS);
+    float cameraDistanceScale = cameraDistance/data.outlineStandardCameraDistance;
+    float3 biasDirWS = normalize(vertexInput.positionWS - cameraPostionWS);
+    float3 biasWS = biasDirWS * data.outlineZBias/1000;
+    float3 biasPostionWS = vertexInput.positionWS + biasWS;
+    float3 biasPositionVS = TransformWorldToView(biasPostionWS);
+    // calculate outline position
+    float outlineWidth = data.outlineWidth*_OUTLINE_WIDTH_SCALE*input.color.a*cameraDistanceScale;
+    float outlineMinWidth = (data.outlineWidth-data.outlineWidthRangeOffset)*_OUTLINE_WIDTH_SCALE*input.color.a;
+    float outlineMaxWidth = (data.outlineWidth+data.outlineWidthRangeOffset)*_OUTLINE_WIDTH_SCALE*input.color.a;
+    outlineWidth = clamp(outlineWidth, outlineMinWidth, outlineMaxWidth);
+    float3 outlinePositionVS = biasPositionVS + normalize(normalVS) * outlineWidth;
+    return TransformWViewToHClip(outlinePositionVS);
+}
+
+// OutlineVaryings OutlineVert(OutlineAttributes input)
+// {
+//     OutlineVaryings output;
+//     // fill outline function;
+//     output.uv = input.uv;
+//     return output;
+// }
+
+// half4 OutlineFrag(OutlineVaryings input) : SV_Target
+// {
+//     #if _OUTLINE_OFF
+//         clip(-1);
+//         return 0;
+//     #else
+//         return _OutlineColor;
+//     #endif
+// }
 #endif
